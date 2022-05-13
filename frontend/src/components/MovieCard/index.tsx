f7ccd3a7857ac39783e15ac9c44a8b0b1b656835
Claubert Vinicius <claubertvinicius68@gmail.com>
2022-05-13 16:56:08 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -16,8 +16,7 @@ function MovieCard({movie} : Props) {
       />
       <div className="fmovie-card-bottom-container">
         <h3>{movie.title}</h3>
-        <MovieScore />
-
+        <MovieScore count={movie.count } score={movie.score}/>
         <Link to={`/form/${movie.id}`}>
         <div className="btn btn-primary fmovie-btn">Avaliar</div>
         </Link>

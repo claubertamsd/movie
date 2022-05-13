f36b4b890cd3c29972ff03e6379f29054eb292fb
Claubert Vinicius <claubertvinicius68@gmail.com>
2022-05-13 16:41:42 -0300

    useParams

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,46 +1,14 @@
-import { Link } from 'react-router-dom';
-import './style.css'
+import FormCard from 'components/FormCard';
+import { Link, useParams } from 'react-router-dom';
+import { Movie } from 'types/movie';
+
+
+
 function Form() {
-  const movie = {
-    id: 1,
-    image:
-      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
-    count: 2,
-    score: 4.5,
-  };
+  const params = useParams();
+  
   return (
-    <div className="fmovie-form-container">
-      <img className="fmovie-movie-card-image" src={movie.image} alt="The Witcher" />
-      <div className="fmovie-card-bottom-container">
-        <h3>{movie.title}</h3>
-        <form className="fmovie-form">
-          <div className="form-group fmovie-form-group">
-            <label htmlFor="email">Informe seu email</label>
-            <input type="email" className="form-control" id="email" />
-          </div>
-          <div className="form-group fmovie-form-group">
-            <label htmlFor="score">Informe sua avaliação</label>
-            <select className="form-control" id="score">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-          </div>
-          <div className="fmovie-form-btn-container">
-            <button type="submit" className="btn btn-primary fmovie-btn">
-              Salvar
-            </button>
-          </div>
-        </form>
-        <Link to="/">
-        <button className="btn btn-secondary fmovie-btn-cancel mt-3">Cancelar</button>
-        </Link>
-        
-      </div>
-    </div>
+    <FormCard movieId={`${params.movieId}`}/>
   );
 }
 

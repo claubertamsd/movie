5c8aeb92cf6ac7c8a0b599cb2018e48455d97935
Claubert Vinicius <72041681+claubertamsd@users.noreply.github.com>
2022-05-31 13:46:30 -0300

    Keycloak Security Configure

--- backend/src/main/java/com/dev/movie/controllers/MovieController.java
+++ backend/src/main/java/com/dev/movie/controllers/MovieController.java
@@ -14,22 +14,26 @@ import com.dev.movie.dto.MovieDTO;
 import com.dev.movie.entities.Movie;
 import com.dev.movie.services.MovieService;
 
+import javax.annotation.security.RolesAllowed;
+
 @RestController
 @RequestMapping(value ="/movies")
 public class MovieController {
 	
 	@Autowired
 	private MovieService service;
-	
+
+	@RolesAllowed("user")
 	@GetMapping
-   public Page<MovieDTO> findAll(Pageable pageable){
-	  return service.findAll(pageable);
-  }
-	
+	public Page<MovieDTO> findAll(Pageable pageable){
+		return service.findAll(pageable);
+	}
+
 	@GetMapping(value="/{id}")
-	   public MovieDTO findById(@PathVariable Long id){
-		  return service.findById(id);
-	  }
+	public MovieDTO findById(@PathVariable Long id){
+		return service.findById(id);
+	}
+
 
 	@PostMapping(value="/posts")
 	public MovieDTO save (@RequestBody Movie movie){
